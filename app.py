a883d5056db389f9254ab60f488d1bb93f4b67f8
akshayalix <akshayalix12@gmail.com>
2022-11-13 12:42:25 +0530

    main ui

--- app.py
+++ app.py
@@ -23,4 +23,108 @@ ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\aksha\Desktop\build\assets\frame0")
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
+
+
+
+
+
+######### UI #############
+
+window = Tk()
+
+window.geometry("800x500")
+window.configure(bg = "#2C2C2C")
+
+
+canvas = Canvas(
+    window,
+    bg = "#2C2C2C",
+    height = 500,
+    width = 800,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat"
+)
+button_1.place(
+    x=607.0,
+    y=120.0,
+    width=144.0,
+    height=39.0
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    highlightcolor="#2D2D2D",
+    command=lambda: print("button_2 clicked"),
+    relief="flat"
+)
+button_2.place(
+    x=607.0,
+    y=262.0,
+    width=144.0,
+    height=39.0
+)
+
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_3 clicked"),
+    relief="flat"
+)
+button_3.place(
+    x=607.0,
+    y=405.0,
+    width=144.0,
+    height=39.0
+)
+
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    274.0,
+    282.0,
+    image=image_image_1
+)
+
+image_image_2 = PhotoImage(
+    file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(
+    102.0,
+    38.0,
+    image=image_image_2
+)
+
+image_image_3 = PhotoImage(
+    file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(
+    242.0,
+    91.0,
+    image=image_image_3
+)
+
+window.bind('<Button-1>', keep_flat)
+
+window.resizable(False, False)
+window.mainloop()
+
+############################################
+
 #-----------------------------------
