c9155d6df444bc27e056c66ac27ac14595177e21
akshayalix <akshayalix12@gmail.com>
2022-12-19 10:47:13 +0530

    added -defs

--- launch.py
+++ launch.py
@@ -7,4 +7,20 @@ from time import sleep
 
 # Variables
 
-def 
+def rapoo():
+    rap = r'C:\Program Files (x86)\Rapoo Audio 7.1\Rapoo Audio 7.1.exe'
+    os.startfile(rap)
+
+    # For Rapoo Sound.
+
+def mica():
+    mica = r'C:\Program Files (x86)\Mica For Everyone\MicaForEveryone.exe'
+    os.startfile(mica)
+
+    # For Mica.
+
+def power():
+    toys = r'C:\Program Files\PowerToys\PowerToys.exe'
+    os.startfile(toys)
+
+    # For PowerToys.
